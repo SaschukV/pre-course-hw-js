c8174c5ecbc5ced98e871ebca99a9fef6285038c
SaschukV <v.saschuk@gmail.com>
2023-12-13 13:18:21 +0200

    lesson-02/06.js

---
+++ lesson-02/06.js
@@ -0,0 +1,16 @@
+let passportWithAddress = {
+  name: 'Petr',
+  surname: 'Petrov',
+  address: {
+    country: 'USA',
+    city: 'LA',
+  },
+}
+
+let copyPassportWithAddress = {
+  ...passportWithAddress,
+  address: { ...passportWithAddress.address, city: 'Bobryisk' },
+}
+
+console.log(passportWithAddress.address.city)
+console.log(copyPassportWithAddress.address.city)
